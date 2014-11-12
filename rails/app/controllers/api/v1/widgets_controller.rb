e15e3cdbf32a78fcd94de94e0341afebb5522124
Andreas Franzen <andreas@devil.se>
2014-11-12 08:28:09 +0100

    add inherited resources

--- rails/app/controllers/api/v1/widgets_controller.rb
+++ rails/app/controllers/api/v1/widgets_controller.rb
@@ -1,9 +1,13 @@
-class Api::V1::WidgetsController < ApplicationController
+class Api::V1::WidgetsController < InheritedResources::Base 
+  skip_before_action :verify_authenticity_token
   def index
     render json: Widget.all
   end
 
   def show
     render json: Widget.find(params[:id])
+  end
+  def permitted_params
+      params.permit(:widget => [:title, :configuration_data])
   end
 end
